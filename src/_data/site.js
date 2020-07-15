23eb159d525fa1755bf433ebc6c314f88bd5463a
Phil Hawksworth <phil@netlify.com>
2020-07-15 15:07:15 +0100

    avoid URL keyword collision

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,8 @@
 // Some useful variables to expose to our site templsates
-
 require('dotenv').config();
-const {
-  SITE_URL='https://localhost',
-  TRELLO_BOARD_URL='https://trello.com/b/Zzc0USwZ/hellotrello'
-} = process.env;
-
 
 module.exports = {
   "utm": "?utm_source=github&utm_medium=hellotrello-pnh&utm_campaign=devex",
-  "host": new URL(SITE_URL).host,
-  "trello_board": TRELLO_BOARD_URL
+  "host": new URL(process.env.URL || "https://localhost").host,
+  "trello_board": process.env.TRELLO_BOARD_URL || 'https://trello.com/b/Zzc0USwZ/hellotrello'
 };
